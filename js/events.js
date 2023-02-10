6ba8aef424c561ed12b6d058810b5c7926e3d57e
arifremal <arifremal013@gmail.com>
2023-02-10 14:47:49 +0600

    new file added

--- js/events.js
+++ js/events.js
@@ -1,1 +1,40 @@
-console.log('separate');
+function red(){
+    document.body.style.backgroundColor = 'blue'
+}
+
+
+const yellow = document.getElementById('yellow')
+yellow.onclick =  d;
+
+function d(){
+    document.body.style.backgroundColor = 'yellow'
+}
+
+const p = document.getElementById('make')
+
+p.onclick = function purple(){
+    document.body.style.backgroundColor = 'purple'
+}
+
+// pink
+const pinkish = document.getElementById('pink')
+pinkish.addEventListener('click',pinki)
+
+function pinki(){
+    document.body.style.backgroundColor = 'pink'
+}
+
+// another pink
+
+// const grey = document.getElementById('green');
+
+// grey.addEventListener = ('click', function makeGreen(){
+//     document.body.style.backgroundColor = 'green';
+
+// } )
+
+// final
+document.getElementById('green').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green';
+} )
+
